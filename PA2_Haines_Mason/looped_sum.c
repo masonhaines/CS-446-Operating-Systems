9a3e53dc84163309470044252594b86901386f0c
masonhaines <mhaines@nevada.unr.edu>
2024-02-22 14:06:04 -0800

    program 1, looped summation works
    may need to adjust time that is printed to terminal. may not be exact ms

--- PA2_Haines_Mason/looped_sum.c
+++ PA2_Haines_Mason/looped_sum.c
@@ -1,37 +1,42 @@
 //Mason Haines CS446 Operating Systems 
+#include <bits/types/struct_timeval.h>
 #include <stdio.h>
-// #include <stdlib.h>
-// #include <pthread.h>
-// #include <ctype.h>
-// #include <time.h>
+#include <sys/time.h>
 
-
+// Function prototypes
 int readFile(char file[], int parsedValues[]);
-int sumArray(int[], int);
-
+int sumArray(int parsedValues[], int numOfValues);
 
 int main(int argc, char* argv[]) {
     
-    int sumArray[1000];
+    int numArray[1000];
+    struct timeval startTime, endTime;
+    
+    // Parse file and load into array and return size of array
+    int numOfvalues = readFile("ten.txt", numArray);
+    if (numOfvalues == -1) return 1; // End program with return 1 if file is not found 
 
-    /* FOR TESTING 
-    int count = readFile("ten.txt", sumArray);
-    printf("Values read from file: ");
-    for (int i = 0; i < count; i++) {
-        printf("%d ", sumArray[i]);
-    }
-    printf("\n");
-    printf("total count of numbers in file: %d\n", count); 
-    */
+    gettimeofday(&startTime, NULL); // Start clock for summation time 
+
+    int SUMofFile = sumArray(numArray, numOfvalues); // Find sum of array from values stored in file 
+    printf("Total Sum of Array: %d\n", SUMofFile);
+
+    gettimeofday(&endTime, NULL); // End clock for summation time 
+
+    // Calculate the time taken in milliseconds
+    long seconds = endTime.tv_sec - startTime.tv_sec;
+    long micros = endTime.tv_usec - startTime.tv_usec;
+    double milliseconds = (seconds * 1000) + (double) micros / 1000;
+
+    printf("Time taken (ms): %.6f\n", milliseconds); // Print Total time taken to calculate sum of array
 
     return 0;
 }
 
-
 // Function reads integers from a file and stores them in an array and returns the count of values read.
 int readFile(char txtFile[], int parsedValues[]) {
     FILE *in_file = fopen(txtFile, "r"); // Open the file for reading ONLY using r flag 
-    int counter = 0;
+    int numOfValues = 0; // Number of values counted in the file 
 
     // Check if file exists
     if (in_file == NULL) {
@@ -40,10 +45,21 @@ int readFile(char txtFile[], int parsedValues[]) {
     }
     
     // Read values from the file using fscanf
-    while (fscanf(in_file, "%d", &parsedValues[counter]) == 1) {
-        counter++;
+    while (fscanf(in_file, "%d", &parsedValues[numOfValues]) == 1) {
+        numOfValues++;
     }
     fclose(in_file); // Close the file stream
 
-    return counter; // Return the number of values parsed
+    return numOfValues; // Return the number of values parsed
+}
+
+// Function to find the total sum of all numbers read from file and stored in an array
+int sumArray(int parsedValues[], int numOfValues) {
+    int totalSum = 0; // variable to store summation of all numbers stored in a file 
+
+    for (int i = 0; i < numOfValues; i++) {
+        totalSum += parsedValues[i];
+    }
+
+    return totalSum;
 }
